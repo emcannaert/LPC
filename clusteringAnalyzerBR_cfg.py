06ac9b7ce087d477f3a381f1403dbf70fd068ee6
Ethan Michael Cannaert <ethan.cannaert@cern.ch>
2023-04-11 19:46:45 -0500

    add cfgs

--- clusteringAnalyzerBR_cfg.py
+++ clusteringAnalyzerBR_cfg.py
@@ -7,7 +7,7 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
 process.GlobalTag.globaltag = '106X_upgrade2018_realistic_v16_L1v1'
 
-
+process.options = cms.untracked.PSet( allowUnscheduled = cms.untracked.bool(True) )
 
 ####################### new JEC stuff########################################
 #process.load('JetMETCorrections.Configuration.JetCorrectors_cff')
@@ -19,35 +19,11 @@ from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
 updateJetCollection(
    process,
    jetSource = cms.InputTag('slimmedJetsAK8'),
-   labelName = 'UpdatedJEC',
+   labelName = 'AK8',
    jetCorrections = ('AK8PFPuppi', cms.vstring(['L2Relative', 'L3Absolute', 'L2L3Residual']), 'None'),  # Update: Safe to always add 'L2L3Residual' as MC contains dummy L2L3Residual corrections (always set to 1)
-   postfix = '',
+   postfix = 'UpdatedJEC',
    printWarning = False
 )
-"""
-from PhysicsTools.PatAlgos.tools.jetTools import *
-from RecoBTag.ONNXRuntime.pfDeepBoostedJet_cff import *
-#from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import * #runMetCorAndUncFromMiniAOD
-
-updateJetCollection(
-    process,
-    jetSource = cms.InputTag('slimmedJetsAK8'),
-    pvSource  = cms.InputTag('offlineSlimmedPrimaryVertices'),
-    svSource  = cms.InputTag('slimmedSecondaryVertices'),
-    rParam    = 0.8,
-    jetCorrections = ('AK8PFPuppi', cms.vstring(['L2Relative', 'L3Absolute', 'L2L3Residual']), 'None'),
-    postfix        = '',
-    printWarning   = True
-)
-
-process.JECSequence = cms.Sequence(
-    process.patJetCorrFactors   *
-    process.updatedPatJets      *
-    #process.patJetCorrFactorsTransientCorrected *
-    #process.updatedPatJetsTransientCorrected    *
-    process.selectedUpdatedPatJets
-    )
-"""
 ##############################################################################
 
 process.leptonVeto = cms.EDFilter("leptonVeto",
@@ -57,7 +33,7 @@ metCollection = cms.InputTag("slimmedMETs"),
    tauCollection = cms.InputTag("slimmedTaus")
 )
 process.hadronFilter = cms.EDFilter("hadronFilter",
-   fatJetCollection = cms.InputTag("slimmedJetsAK8"),
+   fatJetCollection = cms.InputTag("updatedPatJetsAK8UpdatedJEC"),#fatJetCollection = cms.InputTag("slimmedJetsAK8"),
 metCollection = cms.InputTag("slimmedMETs"), 
   jetCollection = cms.InputTag("slimmedJets"),
    bits = cms.InputTag("TriggerResults", "", "HLT"),
@@ -65,8 +41,8 @@ metCollection = cms.InputTag("slimmedMETs"),
 process.clusteringAnalyzerAll = cms.EDAnalyzer("clusteringAnalyzerAll",
 runType = cms.string("BRMC"),
 genPartCollection = cms.InputTag("prunedGenParticles"),
- name = cms.string('BESTGraph'),   path = cms.FileInPath('data/constantgraph.pb'), means = cms.FileInPath('data/ScalerParameters_maxAbs_train.txt'),
-   fatJetCollection = cms.InputTag("slimmedJetsAK8"),
+ name = cms.string('BESTGraph'),   path = cms.FileInPath('data/constantgraph.pb'), means = cms.FileInPath('data/ScalerParameters_maxAbs_train.txt'),#fatJetCollection = cms.InputTag("slimmedJetsAK8"),
+   fatJetCollection = cms.InputTag("updatedPatJetsAK8UpdatedJEC"),
    jetCollection = cms.InputTag("slimmedJets"),
    bits = cms.InputTag("TriggerResults", "", "HLT"),
    muonCollection= cms.InputTag("slimmedMuons"),
@@ -98,16 +74,6 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
-"""process.printTree = cms.EDAnalyzer("ParticleTreeDrawer",
-   src = cms.InputTag("prunedGenParticles"),
-   printP4 = cms.untracked.bool(False),
-   printPtEtaPhi = cms.untracked.bool(False),
-   printVertex = cms.untracked.bool(False),
-   printStatus = cms.untracked.bool(True),
-   printIndex = cms.untracked.bool(False),
-   #status = cms.untracked.vint32(3),
-)
-"""
 process.p = cms.Path( process.hadronFilter * process.leptonVeto * 
 process.clusteringAnalyzerAll  #needs star  process.JECSequence*
    # 
